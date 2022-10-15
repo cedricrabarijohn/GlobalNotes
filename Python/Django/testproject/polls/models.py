a71b9f4e72bf8969a6d5d85d1c8471f739351b52
Cedric <95499695+CedricRabarijohn@users.noreply.github.com>
2022-10-15 16:06:11 +0300

    Modifying testproject

--- Python/Django/testproject/polls/models.py
+++ Python/Django/testproject/polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_txt = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- Python/Django/testproject/testproject/settings.py
+++ Python/Django/testproject/testproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["localhost"]
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,9 +81,9 @@ DATABASES = {
     # }
     'default':{
         'ENGINE' : 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD':'',
+        'NAME': 'cedric',
+        'USER': 'cedric',
+        'PASSWORD':'1234',
         'HOST':'127.0.0.1',
         'PORT':'5433'
     }
